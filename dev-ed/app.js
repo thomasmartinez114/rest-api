b2d50b700cad129300a47180845505de340dc5c2
tmartinez114 <tmartinez114@gmail.com>
2019-12-02 22:34:17 -0500

    created route for post

--- dev-ed/app.js
+++ dev-ed/app.js
@@ -3,15 +3,14 @@ const app = express();
 const mongoose = require('mongoose');
 require('dotenv/config');
 
-// Middlewares
-//     function that executes when routes are hit
+// Import routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
 
 // ROUTES
 app.get('/', (req, res) => {
   res.send('We are on home');
-});
-app.get('/posts', (req, res) => {
-  res.send('We are on posts');
 });
 
 // Connect to DB
