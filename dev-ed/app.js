9707939fc71c3bfa8c4d80cd1da7182e33f015a5
tmartinez114 <tmartinez114@gmail.com>
2019-12-02 21:23:58 -0500

    added posts route

--- dev-ed/app.js
+++ dev-ed/app.js
@@ -2,9 +2,18 @@ const express = require('express');
 
 const app = express();
 
+// Middlewares
+//     function that executes when routes are hit
+app.use('/posts', () => {
+  console.log('This is a middleware running');
+});
+
 // ROUTES
 app.get('/', (req, res) => {
-    res.send('We are on home');
+  res.send('We are on home');
+});
+app.get('/posts', (req, res) => {
+  res.send('We are on posts');
 });
 
 // How do we start listening to the server
