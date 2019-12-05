a35d30444458c76a3a9a1d7147cff4771cc20e5b
tmartinez114 <tmartinez114@gmail.com>
2019-12-04 20:54:16 -0500

    added author in Post

--- dev-ed/models/Post.js
+++ dev-ed/models/Post.js
@@ -9,6 +9,10 @@ const PostSchema = mongoose.Schema({
     type: String,
     required: true
   },
+  author: {
+    type: String,
+    required: true
+  },
   date: {
     type: Date,
     default: Date.now
