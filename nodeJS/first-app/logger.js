c9696e4d7d19b6dc8f99ba7a5c54a7e3bba9d146
tmartinez114 <tmartinez114@gmail.com>
2019-12-23 22:06:53 -0500

    Displaying filename & dirname - Module Wrapper Function

--- nodeJS/first-app/logger.js
+++ nodeJS/first-app/logger.js
@@ -1,12 +1,11 @@
-(function(exports, require, module, __filename, __dirname) {
-  console.log(__filename);
-  console.log(__dirname);
-  var url = 'http://mylogger.io/log';
+console.log(__filename);
+console.log(__dirname);
 
-  function log(message) {
-    // Send an HTTP request
-    console.log(message);
-  }
+var url = 'http://mylogger.io/log';
 
-  module.exports = log;
-});
+function log(message) {
+  // Send an HTTP request
+  console.log(message);
+}
+
+module.exports = log;
