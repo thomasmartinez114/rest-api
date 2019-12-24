c65b416069f7fe059964cf3ba72fa3262b76d974
tmartinez114 <tmartinez114@gmail.com>
2019-12-23 22:23:03 -0500

    OS Module - Using ES6

--- nodeJS/first-app/app.js
+++ nodeJS/first-app/app.js
@@ -1,5 +1,12 @@
-const path = require('path');
+const os = require('os');
 
-var pathObj = path.parse(__filename);
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
 
-console.log(pathObj);
+// console.log('Total Memory: ' + totalMemory);
+
+// Template string
+// ES6 / ES2015 : ECMAScript 6
+
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
