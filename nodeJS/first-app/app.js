2be280e7501faadfcf0b98441a18f6adbd914f7d
tmartinez114 <tmartinez114@gmail.com>
2019-12-23 23:28:02 -0500

    Events Module

--- nodeJS/first-app/app.js
+++ nodeJS/first-app/app.js
@@ -1,9 +1,10 @@
-const fs = require('fs');
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
-// const files = fs.readdirSync('./');
-// console.log(files);
-
-fs.readdir('./', function(err, files) {
-  if (err) console.log('Error', err);
-  else console.log('Result', files);
+// Register a listener
+emitter.on('messageLogged', function() {
+  console.log('Listener called');
 });
+
+emitter.emit('messageLogged');
+// Making a noise, produce - signaling and event happening
