738526d5b28f29e62b09bedd8c951da32be8f7b1
tmartinez114 <tmartinez114@gmail.com>
2019-12-26 21:53:38 -0500

    Handling GET requests

--- nodeJS/express-demo/index.js
+++ nodeJS/express-demo/index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' }
+];
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+// Get all courses
 app.get('/api/courses', (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
-// /api/courses/1
-app.get('/api/courses/:year/:month', (req, res) => {
-  res.send(req.params.year);
+// Get one course by id
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  // 404 = NOT FOUND
+  if (!course) res.status(404).send('The course with the given ID was not found!');
+  res.send(course);
 });
 
 // PORT
