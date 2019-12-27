a3e4c49ac51b0585452178db62208141dd21a09e
tmartinez114 <tmartinez114@gmail.com>
2019-12-26 22:09:03 -0500

    validating input with if statement

--- nodeJS/express-demo/index.js
+++ nodeJS/express-demo/index.js
@@ -21,6 +21,12 @@ app.get('/api/courses', (req, res) => {
 
 // POST
 app.post('/api/courses', (req, res) => {
+  if (!req.body.name || req.body.name.length < 3) {
+    // 400 Bad Request
+    res.status(400).send('Name is required and should have atleast 3 characters');
+    return;
+  }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name
