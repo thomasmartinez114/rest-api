9a331843266e9eebe1adf807b7004176db7c9968
tmartinez114 <tmartinez114@gmail.com>
2020-01-01 16:50:11 -0500

    Patters for dealing with async // made function to place the setTimeout() in

--- nodeJS/async-demo/index.js
+++ nodeJS/async-demo/index.js
@@ -1,8 +1,19 @@
 console.log('Before');
-setTimeout(() => {
-  console.log('Reading a user from a database...');
-}, 2000);
+getUser(1);
 console.log('After');
+
+// Callbacks
+// Promises
+// Async/await
+
+function getUser(id) {
+  setTimeout(() => {
+    console.log('Reading a user from a database...');
+    return { id: id, getHubUsername: 'mosh' };
+  }, 2000);
+
+  return 1;
+}
 
 // Will Log
 // Before
