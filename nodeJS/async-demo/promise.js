22a8963ef40d64aa2e6c0a560e7e1b22ceac9a00
tmartinez114 <tmartinez114@gmail.com>
2020-01-04 12:05:55 -0500

    did a rejection setTimeout() and added notes on bottom of the code

--- nodeJS/async-demo/promise.js
+++ nodeJS/async-demo/promise.js
@@ -2,9 +2,16 @@ const p = new Promise((resolve, reject) => {
   // Kick off some async work
   // ...
   setTimeout(() => {
-    resolve(1);
+    // resolve(1);
+    reject(new Error('message'));
   }, 2000);
-  //   reject(new Error('message'));
 });
 
-p.then(result => console.log('Result', result));
+p.then(result => console.log('Result', result)).catch(err => console.log('Error', err.message));
+
+// .then for the successfully
+// .catch for error
+
+// Promise holds eventual result of async operation
+// if passes - pending => resolved, fullfilled
+// if failes - pending => rejected
