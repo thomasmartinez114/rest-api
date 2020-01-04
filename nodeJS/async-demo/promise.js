63d0d85a673e8af92404f6fa588bfcefa1bfd51e
tmartinez114 <tmartinez114@gmail.com>
2020-01-04 12:02:36 -0500

    added setTimeout() for 2 seconds

--- nodeJS/async-demo/promise.js
+++ nodeJS/async-demo/promise.js
@@ -1,7 +1,9 @@
 const p = new Promise((resolve, reject) => {
   // Kick off some async work
   // ...
-  resolve(1);
+  setTimeout(() => {
+    resolve(1);
+  }, 2000);
   //   reject(new Error('message'));
 });
 
