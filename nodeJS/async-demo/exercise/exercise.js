f7b56729d0cc9cd8f57072f4e01bad2cf4ba0481
tmartinez114 <tmartinez114@gmail.com>
2020-01-05 01:13:43 -0500

    Modified functions to return Promises

--- nodeJS/async-demo/exercise/exercise.js
+++ nodeJS/async-demo/exercise/exercise.js
@@ -1,3 +1,12 @@
+// Async and Await
+async function custoerInfo() {
+  try {
+    const customer = await getCustomer;
+  } catch (err) {
+    console.log('Error', err.message);
+  }
+}
+
 getCustomer(1, customer => {
   console.log('Customer: ', customer);
   if (customer.isGold) {
@@ -10,25 +19,33 @@ getCustomer(1, customer => {
   }
 });
 
-function getCustomer(id, callback) {
-  setTimeout(() => {
-    callback({
-      id: 1,
-      name: 'Mosh Hamedani',
-      isGold: true,
-      email: 'email'
-    });
-  }, 4000);
+// Change functions to have promise
+
+function getCustomer(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        id: 1,
+        name: 'Mosh Hamedani',
+        isGold: true,
+        email: 'email'
+      });
+    }, 4000);
+  });
 }
 
-function getTopMovies(callback) {
-  setTimeout(() => {
-    callback(['movie1', 'movie2']);
-  }, 4000);
+function getTopMovies() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(['movie1', 'movie2']);
+    }, 4000);
+  });
 }
 
-function sendEmail(email, movies, callback) {
-  setTimeout(() => {
-    callback();
-  }, 4000);
+function sendEmail(email, movies) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 4000);
+  });
 }
