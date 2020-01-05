bf2ce0996bf9de4daa32cfd787fd85c963bccc71
tmartinez114 <tmartinez114@gmail.com>
2020-01-05 00:36:02 -0500

    Parallel Promises - one rejected

--- nodeJS/async-demo/promise-api.js
+++ nodeJS/async-demo/promise-api.js
@@ -1,7 +1,7 @@
-const p1 = new Promise(resolve => {
+const p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
     console.log('Async operation 1...');
-    resolve(1);
+    reject(new Error('Because something failed.'));
   }, 2000);
 });
 
@@ -12,4 +12,6 @@ const p2 = new Promise(resolve => {
   }, 2000);
 });
 
-Promise.all([p1, p2]).then(result => console.log(result));
+Promise.all([p1, p2])
+  .then(result => console.log(result))
+  .catch(err => console.log('Error', err.message));
