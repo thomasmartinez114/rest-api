e4f85a87ed33ee5ce63c7e70436c84c888a599ad
tmartinez114 <tmartinez114@gmail.com>
2020-01-05 01:18:03 -0500

    Async function to make easier to read

--- nodeJS/async-demo/exercise/exercise.js
+++ nodeJS/async-demo/exercise/exercise.js
@@ -1,26 +1,28 @@
-// Async and Await
-async function custoerInfo() {
-  try {
-    const customer = await getCustomer;
-  } catch (err) {
-    console.log('Error', err.message);
-  }
-}
+// getCustomer(1, customer => {
+//   console.log('Customer: ', customer);
+//   if (customer.isGold) {
+//     getTopMovies(movies => {
+//       console.log('Top movies: ', movies);
+//       sendEmail(customer.email, movies, () => {
+//         console.log('Email sent...');
+//       });
+//     });
+//   }
+// });
 
-getCustomer(1, customer => {
+async function notifyCustomer() {
+  const customer = await getCustomer(1);
   console.log('Customer: ', customer);
   if (customer.isGold) {
-    getTopMovies(movies => {
-      console.log('Top movies: ', movies);
-      sendEmail(customer.email, movies, () => {
-        console.log('Email sent...');
-      });
-    });
+    const movies = await getTopMovies();
+    console.log('Top movies: ', movies);
+    await sendEmail(customer.email, movies);
+    console.log('Email sent....');
   }
-});
+}
+notifyCustomer();
 
 // Change functions to have promise
-
 function getCustomer(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
