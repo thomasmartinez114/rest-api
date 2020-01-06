efaedde1f1932b29693a3633d211a1ecd5dbb3bc
tmartinez114 <tmartinez114@gmail.com>
2020-01-05 20:26:21 -0500

    >= 10 <= 20

--- nodeJS/mongo-demo/index.js
+++ nodeJS/mongo-demo/index.js
@@ -43,7 +43,7 @@ async function getCourses() {
 
   const courses = await Course
     // .find({ author: 'Mosh', isPublished: true })
-    .find({ price: { $gte: 10 } })
+    .find({ price: { $gte: 10, $lte: 20 } })
     .limit(10)
     .sort({ name: 1 }) // 1 ascending, -1 descending
     .select({ name: 1, tags: 1 });
