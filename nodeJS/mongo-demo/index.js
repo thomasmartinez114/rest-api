1ec9392dd9627af3699c55f67c755da65cba5c32
tmartinez114 <tmartinez114@gmail.com>
2020-01-05 20:01:13 -0500

    Finding price thats >= 10 // using $ to indicator the operator

--- nodeJS/mongo-demo/index.js
+++ nodeJS/mongo-demo/index.js
@@ -31,7 +31,19 @@ async function createCourse() {
 }
 
 async function getCourses() {
-  const courses = await Course.find({ author: 'Mosh', isPublished: true })
+  // Comparasion operators
+  // eq (equal)
+  // ne (not equal)
+  // gt (greater than)
+  // gte (greater than or equal to)
+  // lt (less than)
+  // lte (less than or equal to)
+  // in
+  // nin (not in)
+
+  const courses = await Course
+    // .find({ author: 'Mosh', isPublished: true })
+    .find({ price: { $gte: 10 } })
     .limit(10)
     .sort({ name: 1 }) // 1 ascending, -1 descending
     .select({ name: 1, tags: 1 });
