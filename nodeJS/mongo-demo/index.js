d09713d43645c5db14fa7af22113a4471991344f
tmartinez114 <tmartinez114@gmail.com>
2020-01-05 20:27:59 -0500

    Completed COMPARASION OPERATORS // .find 10, 15, 20

--- nodeJS/mongo-demo/index.js
+++ nodeJS/mongo-demo/index.js
@@ -43,7 +43,8 @@ async function getCourses() {
 
   const courses = await Course
     // .find({ author: 'Mosh', isPublished: true })
-    .find({ price: { $gte: 10, $lte: 20 } })
+    // .find({ price: { $gte: 10, $lte: 20 } })
+    .find({ price: { $in: [10, 15, 20] } })
     .limit(10)
     .sort({ name: 1 }) // 1 ascending, -1 descending
     .select({ name: 1, tags: 1 });
