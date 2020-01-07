03bfbc0d63465fd84e66d8369bca58c9e686d7eb
tmartinez114 <tmartinez114@gmail.com>
2020-01-06 21:54:31 -0500

    Set up course Schema

--- nodeJS/mongo-demo/exercise/7-14/solution.js
+++ nodeJS/mongo-demo/exercise/7-14/solution.js
@@ -10,3 +10,12 @@ mongoose
   .connect('mongodb://localhost/mongo-exercises')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+  price: Number
+});
