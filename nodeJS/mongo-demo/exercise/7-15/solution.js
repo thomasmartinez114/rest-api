7fa5c231eb4c84c738c6a2351de2294ad5aa3258
tmartinez114 <tmartinez114@gmail.com>
2020-01-06 22:50:03 -0500

    resolved using tags and descending price order

--- nodeJS/mongo-demo/exercise/7-15/solution.js
+++ nodeJS/mongo-demo/exercise/7-15/solution.js
@@ -1,36 +1,30 @@
-// Get all published frontend and backend courses
-// sort them by their price in descending order
+// All frontend and backend
+// sort by price descending
 // pick only their name and author
 // display
 
 const mongoose = require('mongoose');
 
-// Connect to DB
-mongoose
-  .connect('mongodb://localhost/mongo-exercises')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...', err));
+mongoose.connect('mongodb://localhost/mongo-exercises');
 
 const courseSchema = new mongoose.Schema({
   name: String,
   author: String,
   tags: [String],
-  date: { type: Date, default: Date.now },
+  date: Date,
   isPublished: Boolean,
   price: Number
 });
 
-// Model
 const Course = mongoose.model('Course', courseSchema);
 
-// Querying the databases
 async function getCourses() {
-  return await Course.find({ isPublished: true, tags: { $in: ['frontend', 'backend'] } })
+  return await Course.find({ isPublished: true })
+    .or([{ tags: 'frontend' }, { tags: 'backend' }])
     .sort('-price')
     .select('name author price');
 }
 
-// Running the query
 async function run() {
   const courses = await getCourses();
   console.log(courses);
