a8b28df7a6496f64c234b463edd762684e81d282
tmartinez114 <tmartinez114@gmail.com>
2020-01-06 23:01:43 -0500

    Updating DB - query first

--- nodeJS/mongo-demo/index.js
+++ nodeJS/mongo-demo/index.js
@@ -43,3 +43,28 @@ async function getCourses() {
 }
 
 getCourses();
+
+async function updateCourse(id) {
+  // Approach: Query First
+  // findById()
+  // Modify its properties
+  // save()
+
+  const course = await Course.findById(id);
+  if (!course) return;
+
+  // Setting the queries in 2 diff ways
+  // First way
+  course.isPublished = true;
+  course.author = 'Another Author';
+  //Second way
+  // course.set({
+  //   isPublished: true,
+  //   author: 'Another Author'
+  // });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+updateCourse('5e127d9446eb8552a87fc383');
