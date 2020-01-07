7ebeb3e420f1accd72199b46118c8cff4aec1fd8
tmartinez114 <tmartinez114@gmail.com>
2020-01-06 22:50:40 -0500

    completed 7-14

--- nodeJS/mongo-demo/exercise/7-14/solution.js
+++ nodeJS/mongo-demo/exercise/7-14/solution.js
@@ -1,36 +1,24 @@
-// Get all the published backend courses
-// Sort them by their name
-// Pick only the name and author
-// Display them
-
 const mongoose = require('mongoose');
 
-// Connect to DB
-mongoose
-  .connect('mongodb://localhost/mongo-exercises')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...', err));
+mongoose.connect('mongodb://localhost/mongo-exercises');
 
 const courseSchema = new mongoose.Schema({
   name: String,
   author: String,
   tags: [String],
-  date: { type: Date, default: Date.now },
+  date: Date,
   isPublished: Boolean,
   price: Number
 });
 
-// Model
 const Course = mongoose.model('Course', courseSchema);
 
-// Querying the databases
 async function getCourses() {
   return await Course.find({ isPublished: true, tags: 'backend' })
-    .sort('name')
-    .select('name author');
+    .sort({ name: 1 })
+    .select({ name: 1, author: 1 });
 }
 
-// Running the query
 async function run() {
   const courses = await getCourses();
   console.log(courses);
