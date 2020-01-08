3b4586483b4e599d13542e7d80c44330ea51290a
tmartinez114 <tmartinez114@gmail.com>
2020-01-08 18:09:54 -0500

    Installed mongoose // connected index.js to mongoose

--- nodeJS/vidly/index.js
+++ nodeJS/vidly/index.js
@@ -1,7 +1,14 @@
+const mongoose = require('mongoose');
 const Joi = require('joi');
 const express = require('express');
 const genres = require('./routes/genres');
 const app = express();
+
+// Connect to Mongoose
+mongoose
+  .connect('mongodb://localhost/vidly-db')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.err('Could not connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
