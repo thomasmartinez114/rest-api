6a08b7c3bdcd4e21d8dd6116f9b7027acc3e374f
tmartinez114 <tmartinez114@gmail.com>
2020-01-08 18:23:59 -0500

    posting genres

--- nodeJS/vidly/routes/genres.js
+++ nodeJS/vidly/routes/genres.js
@@ -21,15 +21,14 @@ router.get('/', async (req, res) => {
   res.send(genres);
 });
 
+// Posting genres
 router.post('/', (req, res) => {
   const { error } = validateGenre(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name
-  };
-  genres.push(genre);
+  let genre = new Genre({ name: req.body.name });
+  genre = await genre.save();
+
   res.send(genre);
 });
 
