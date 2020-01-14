200baba01680193462fd44015e388bfd86779fe6
tmartinez114 <tmartinez114@gmail.com>
2020-01-13 20:01:38 -0500

    Rental API - rental.js route

---
+++ nodeJS/vidly/routes/rentals.js
@@ -0,0 +1,48 @@
+const { Rental, validate } = require('../models/rental');
+const { Movie } = require('../models/movie');
+const { Customer } = require('../models/customer');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const rentals = await Rental.find().sort('-dateOut'); // dateOut desc sort
+  res.send(rentals);
+});
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Find customer by id
+  const customer = await Customer.findById(req.body.customerId);
+  // If doesn't exist - error
+  if (!customer) return res.status(400).send('Invalid customer.');
+
+  // Find movie by id
+  const movie = await Movie.findById(req.body.movieId);
+  // If doesn't exist - error
+  if (!movie) return res.status(400).send('Invalid movie.');
+
+  // If movie is not in stock - error
+  if (movie.numberInStock === 0) return res.status(400).send('Movie not in stock.');
+
+  let rental = new Rental({
+      customer: {
+          _id: customer:_id,
+          name: customer.name,
+          phone: customer.phone
+      },
+      movie: {
+          _id: movie._id,
+          title: movie.title,
+          dailyRentalRate: movie.dailyRentalRate
+      }
+  });
+  rental = await rental.save();
+
+  movie.numberInStock--; // decrement in stock
+  movie.save();
+
+  res.send(rental);
+});
