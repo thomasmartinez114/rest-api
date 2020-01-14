b63541b836b17b156d10dfaebab5c58767382613
tmartinez114 <tmartinez114@gmail.com>
2020-01-14 18:39:36 -0500

    installed Fawn - initiliazed in rentals.js route

--- nodeJS/vidly/routes/rentals.js
+++ nodeJS/vidly/routes/rentals.js
@@ -1,9 +1,12 @@
-const {Rental, validate} = require('../models/rental'); 
-const {Movie} = require('../models/movie'); 
-const {Customer} = require('../models/customer'); 
+const { Rental, validate } = require('../models/rental');
+const { Movie } = require('../models/movie');
+const { Customer } = require('../models/customer');
 const mongoose = require('mongoose');
+const Fawn = require('fawn');
 const express = require('express');
 const router = express.Router();
+
+Fawn.init(mongoose);
 
 router.get('/', async (req, res) => {
   const rentals = await Rental.find().sort('-dateOut');
@@ -11,7 +14,7 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const { error } = validate(req.body); 
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const customer = await Customer.findById(req.body.customerId);
@@ -22,10 +25,10 @@ router.post('/', async (req, res) => {
 
   if (movie.numberInStock === 0) return res.status(400).send('Movie not in stock.');
 
-  let rental = new Rental({ 
+  let rental = new Rental({
     customer: {
       _id: customer._id,
-      name: customer.name, 
+      name: customer.name,
       phone: customer.phone
     },
     movie: {
@@ -38,7 +41,7 @@ router.post('/', async (req, res) => {
 
   movie.numberInStock--;
   movie.save();
-  
+
   res.send(rental);
 });
 
@@ -50,4 +53,4 @@ router.get('/:id', async (req, res) => {
   res.send(rental);
 });
 
-module.exports = router; 
+module.exports = router;
