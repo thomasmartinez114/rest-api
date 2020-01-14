e31de677996b4aa564647e32808844ec470898cd
tmartinez114 <tmartinez114@gmail.com>
2020-01-13 19:22:35 -0500

    Project - Movie API

--- nodeJS/vidly/index.js
+++ nodeJS/vidly/index.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
-const express = require('express');
-const customers = require('./routes/customer');
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+const express = require('express');
 const app = express();
 
-// Connect to Mongoose
 mongoose
   .connect('mongodb://localhost/vidly-db')
   .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.err('Could not connect to MongoDB...'));
+  .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- nodeJS/vidly/routes/customers.js
+++ nodeJS/vidly/routes/customers.js
@@ -1,4 +1,4 @@
-const { Customer, validate } = require('../models/customer');
+const {Customer, validate} = require('../models/customer'); 
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
@@ -9,35 +9,32 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const { error } = validate(req.body);
+  const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
-  let customer = new Customer({
+  let customer = new Customer({ 
     name: req.body.name,
     isGold: req.body.isGold,
     phone: req.body.phone
   });
   customer = await customer.save();
-
+  
   res.send(customer);
 });
 
 router.put('/:id', async (req, res) => {
-  const { error } = validate(req.body);
+  const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
-  const customer = await Customer.findByIdAndUpdate(
-    req.params.id,
-    {
+  const customer = await Customer.findByIdAndUpdate(req.params.id,
+    { 
       name: req.body.name,
       isGold: req.body.isGold,
       phone: req.body.phone
-    },
-    { new: true }
-  );
+    }, { new: true });
 
   if (!customer) return res.status(404).send('The customer with the given ID was not found.');
-
+  
   res.send(customer);
 });
 
@@ -57,4 +54,4 @@ router.get('/:id', async (req, res) => {
   res.send(customer);
 });
 
-module.exports = router;
+module.exports = router; 
