2b0d0f63bf72e1e7a6a0411149896036289a35e9
tmartinez114 <tmartinez114@gmail.com>
2020-01-13 20:08:40 -0500

    added Moshs code to directory to resolve issue i was having

--- nodeJS/vidly/routes/rentals.js
+++ nodeJS/vidly/routes/rentals.js
@@ -1,58 +1,53 @@
-const { Rental, validate } = require('../models/rental');
-const { Movie } = require('../models/movie');
-const { Customer } = require('../models/customer');
+const {Rental, validate} = require('../models/rental'); 
+const {Movie} = require('../models/movie'); 
+const {Customer} = require('../models/customer'); 
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const rentals = await Rental.find().sort('-dateOut'); // dateOut desc sort
+  const rentals = await Rental.find().sort('-dateOut');
   res.send(rentals);
 });
 
 router.post('/', async (req, res) => {
-  const { error } = validate(req.body);
+  const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
-  // Find customer by id
   const customer = await Customer.findById(req.body.customerId);
-  // If doesn't exist - error
   if (!customer) return res.status(400).send('Invalid customer.');
 
-  // Find movie by id
   const movie = await Movie.findById(req.body.movieId);
-  // If doesn't exist - error
   if (!movie) return res.status(400).send('Invalid movie.');
 
-  // If movie is not in stock - error
   if (movie.numberInStock === 0) return res.status(400).send('Movie not in stock.');
 
-  let rental = new Rental({
-      customer: {
-          _id: customer:_id,
-          name: customer.name,
-          phone: customer.phone
-      },
-      movie: {
-          _id: movie._id,
-          title: movie.title,
-          dailyRentalRate: movie.dailyRentalRate
-      }
+  let rental = new Rental({ 
+    customer: {
+      _id: customer._id,
+      name: customer.name, 
+      phone: customer.phone
+    },
+    movie: {
+      _id: movie._id,
+      title: movie.title,
+      dailyRentalRate: movie.dailyRentalRate
+    }
   });
   rental = await rental.save();
 
-  movie.numberInStock--; // decrement in stock
+  movie.numberInStock--;
   movie.save();
-
+  
   res.send(rental);
 });
 
 router.get('/:id', async (req, res) => {
-    const rental = await Rental.findById(req.params.id);
+  const rental = await Rental.findById(req.params.id);
 
-    if (!rental) return res.status(404).send('The rental with the given ID was not found.');
+  if (!rental) return res.status(404).send('The rental with the given ID was not found.');
 
-    res.send(rental);
-})
+  res.send(rental);
+});
 
-module.exports = router;
+module.exports = router; 
