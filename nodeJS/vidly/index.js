4f951ed7de614e0ae37768968b1e593f227106da
tmartinez114 <tmartinez114@gmail.com>
2020-01-20 15:18:17 -0500

    Registering Users

--- nodeJS/vidly/index.js
+++ nodeJS/vidly/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const users = require('./routes/users');
 const express = require('express');
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ nodeJS/vidly/routes/users.js
@@ -0,0 +1,25 @@
+const { Genre, validate } = require('../models/user');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //  make sure user isn't already registered
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send('User already registered.');
+
+  user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  await user.save();
+
+  res.send(user);
+});
+
+module.exports = router;
