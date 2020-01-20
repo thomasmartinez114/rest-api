3272eb9e7934fca46e116373e2c8b12be9eee0e8
tmartinez114 <tmartinez114@gmail.com>
2020-01-20 14:15:31 -0500

    notes for 10-01 video

---
+++ nodeJS/notes/x.js
@@ -0,0 +1,15 @@
+// /api/genres
+// /api/movies
+// /api/customers
+// /api/rentals
+
+// Authentication = user and password
+// Authorization = user permissions
+
+// Register: POST /api/users {name, email, password}
+// Login: POST /api/logins
+
+email: {
+    type: String,
+    unique: true
+}
