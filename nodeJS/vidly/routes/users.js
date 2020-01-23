5c6867b0ccc335ffafe57fa5949c5b2e5c8035b4
tmartinez114 <tmartinez114@gmail.com>
2020-01-22 21:54:46 -0500

    Installed Lodash - and utilized to make requesting body simpler

--- nodeJS/vidly/routes/users.js
+++ nodeJS/vidly/routes/users.js
@@ -1,4 +1,5 @@
-const { Genre, validate } = require('../models/user');
+const _ = require('lodash');
+const { User, validate } = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
@@ -11,15 +12,11 @@ router.post('/', async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send('User already registered.');
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  });
+  user = new User(_.pick(req.body, ['name', 'email', 'password']));
 
   await user.save();
 
-  res.send(user);
+  res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
