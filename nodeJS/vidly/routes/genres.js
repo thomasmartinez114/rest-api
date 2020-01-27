a41253e2ee7818c0a5aede77253e5441b474c972
tmartinez114 <tmartinez114@gmail.com>
2020-01-26 22:00:34 -0500

    Role based authorization

--- nodeJS/vidly/routes/genres.js
+++ nodeJS/vidly/routes/genres.js
@@ -1,5 +1,6 @@
 const auth = require('../middleware/auth');
 const { Genre, validate } = require('../models/genre');
+const admin = require('../middleware/admin');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
@@ -36,7 +37,7 @@ router.put('/:id', async (req, res) => {
   res.send(genre);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
