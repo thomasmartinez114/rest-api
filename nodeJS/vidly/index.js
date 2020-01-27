1022abf0a8b6b76df75f5bbbe3235933fde4b2e4
tmartinez114 <tmartinez114@gmail.com>
2020-01-26 23:20:50 -0500

    Logging errors with winston

--- nodeJS/vidly/index.js
+++ nodeJS/vidly/index.js
@@ -1,4 +1,5 @@
 require('express-async-errors');
+const winston = require('winston');
 const Joi = require('joi');
 const config = require('config');
 Joi.objectId = require('joi-objectid')(Joi);
@@ -12,6 +13,8 @@ const users = require('./routes/users');
 const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+
+winston.add(winston.transports.File, { filename: 'logfile.log' });
 
 if (!config.get('jwtPrivateKey')) {
   console.log('FATAL ERROR: jwtPrivateKey is not defined');
@@ -27,6 +30,7 @@ app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 app.use(error);
