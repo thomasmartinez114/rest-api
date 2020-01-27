b87f31f14809ca83fdcc6efac3b1f9dae83688bd
tmartinez114 <tmartinez114@gmail.com>
2020-01-26 22:54:28 -0500

    centralized location for trycatch - created middleware to make code cleaner

--- nodeJS/vidly/routes/genres.js
+++ nodeJS/vidly/routes/genres.js
@@ -1,20 +1,17 @@
 const auth = require('../middleware/auth');
+const asyncMiddleware = require('../middleware/async')
 const { Genre, validate } = require('../models/genre');
 const admin = require('../middleware/admin');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  try {
-    const genres = await Genre.find().sort('name');
-    res.send(genres);
-  } catch (ex) {
-    next(ex);
-  }
+router.get('/', asyncMiddleware (async (req, res) => {
+  const genres = await Genre.find().sort('name');
+  res.send(genres);
 });
 
-router.post('/', auth, async (req, res) => {
+router.post('/', auth, asyncMiddleware (async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -22,7 +19,7 @@ router.post('/', auth, async (req, res) => {
   genre = await genre.save();
 
   res.send(genre);
-});
+}));
 
 router.put('/:id', async (req, res) => {
   const { error } = validate(req.body);
