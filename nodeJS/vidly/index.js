f8946053c60c39f99a4e5c169f78c8705a6d0cff
tmartinez114 <tmartinez114@gmail.com>
2020-01-27 23:16:31 -0500

    refactored index.js - logging

--- nodeJS/vidly/index.js
+++ nodeJS/vidly/index.js
@@ -1,29 +1,12 @@
-require('express-async-errors');
-const winston = require('winston');
-require('winston-mongodb');
 const Joi = require('joi');
 const config = require('config');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
 
+require('./startup/logging');
 require('./startup/routes')(app);
 require('./startup/db')();
-
-winston.handleExceptions(new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
-
-process.on('unhandledRejection', ex => {
-  throw ex;
-});
-
-winston.add(winston.transports.File, { filename: 'logfile.log' });
-winston.add(winston.transports.MongoDB, {
-  db: 'mongodb://localhost/vidly',
-  level: 'error'
-});
-
-const p = Promise.reject(new Error('Something failed miserably!'));
-p.then(() => console.log('Done'));
 
 if (!config.get('jwtPrivateKey')) {
   console.log('FATAL ERROR: jwtPrivateKey is not defined');
