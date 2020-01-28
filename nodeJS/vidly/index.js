2ded8298326c157b6deda60953e1edd0bd7bece3
tmartinez114 <tmartinez114@gmail.com>
2020-01-27 23:09:24 -0500

    refactored index.js - routes

--- nodeJS/vidly/index.js
+++ nodeJS/vidly/index.js
@@ -5,15 +5,10 @@ const Joi = require('joi');
 const config = require('config');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const error = require('./middleware/error');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+
+require('./startup/routes')(app);
 
 winston.handleExceptions(new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
 
@@ -39,15 +34,6 @@ mongoose
   .connect('mongodb://localhost/vidly-db')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
