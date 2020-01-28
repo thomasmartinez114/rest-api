54cdea006b3a8c7263a0f4150caa99abe17f4640
tmartinez114 <tmartinez114@gmail.com>
2020-01-27 23:23:15 -0500

    adding color to the error message

--- nodeJS/vidly/startup/logging.js
+++ nodeJS/vidly/startup/logging.js
@@ -3,7 +3,10 @@ require('winston-mongodb');
 require('express-async-errors');
 
 module.exports = function() {
-  winston.handleExceptions(new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
+  winston.handleExceptions(
+    new winston.transports.Console({ colorize: true, prettyPrint: true }),
+    new winston.transports.File({ filename: 'uncaughtExceptions.log' })
+  );
 
   process.on('unhandledRejection', ex => {
     throw ex;
