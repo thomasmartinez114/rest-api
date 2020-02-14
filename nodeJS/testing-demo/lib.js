ce28f29674a9ee52f04502c44c28e780d084055c
tmartinez114 <tmartinez114@gmail.com>
2020-02-13 21:04:13 -0600

    passed test for greet()

--- nodeJS/testing-demo/lib.js
+++ nodeJS/testing-demo/lib.js
@@ -2,14 +2,14 @@ const db = require('./db');
 const mail = require('./mail');
 
 // Testing numbers
-module.exports.absolute = function(number) {
-  return number >= 0 ? number : -number;
-};
+// module.exports.absolute = function(number) {
+//   return number >= 0 ? number : -number;
+// };
 
-// // Testing strings
-// module.exports.greet = function(name) {
-//   return 'Welcome ' + name;
-// }
+// Testing strings
+module.exports.greet = function(name) {
+  return 'Welcome ' + name;
+};
 
 // // Testing arrays
 // module.exports.getCurrencies = function() {
--- nodeJS/testing-demo/tests/lib.test.js
+++ nodeJS/testing-demo/tests/lib.test.js
@@ -1,18 +1,25 @@
 const lib = require('../lib');
 
-describe('absolute', () => {
-  it('should return a positive number if input is positive', () => {
-    const result = lib.absolute(1);
-    expect(result).toBe(1);
-  });
+// describe('absolute', () => {
+//   it('should return a positive number if input is positive', () => {
+//     const result = lib.absolute(1);
+//     expect(result).toBe(1);
+//   });
 
-  it('should return a positive number if input is negative', () => {
-    const result = lib.absolute(-1);
-    expect(result).toBe(1);
-  });
+//   it('should return a positive number if input is negative', () => {
+//     const result = lib.absolute(-1);
+//     expect(result).toBe(1);
+//   });
 
-  it('should return 0 if input is 0', () => {
-    const result = lib.absolute(0);
-    expect(result).toBe(0);
+//   it('should return 0 if input is 0', () => {
+//     const result = lib.absolute(0);
+//     expect(result).toBe(0);
+//   });
+// });
+
+describe('greet', () => {
+  it('should return the greeting message', () => {
+    const result = lib.greet('Mosh');
+    expect(result).toBe('Welcome Mosh');
   });
 });
