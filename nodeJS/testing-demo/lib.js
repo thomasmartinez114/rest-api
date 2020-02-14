6f227d37bb1097b670c4688b0a870c058f98aa82
tmartinez114 <tmartinez114@gmail.com>
2020-02-13 22:59:25 -0600

    alignment

--- nodeJS/testing-demo/lib.js
+++ nodeJS/testing-demo/lib.js
@@ -18,7 +18,7 @@ const mail = require('./mail');
 
 // Testing objects
 module.exports.getProduct = function(productId) {
-  return { id: productId, price: 10 };
+  return { id: productId, price: 10, category: 'Funny' };
 };
 
 // // Testing exceptions
