c06415252db125612964ae5148f8bf51037c1561
tmartinez114 <tmartinez114@gmail.com>
2020-02-14 12:51:33 -0600

    Check arugments being passed through method - Exact match & RegEx

--- nodeJS/testing-demo/tests/lib.test.js
+++ nodeJS/testing-demo/tests/lib.test.js
@@ -100,6 +100,8 @@ describe('notifyCustomer', () => {
 
     lib.notifyCustomer({ customerId: 1 });
 
-    expect(mail.send).toHaveBennCalledWith('a', '...');
+    expect(mail.send).toHaveBeenCalled();
+    expect(mail.send.mock.calls[0][0]).toBe('a');
+    expect(mail.send.mock.calls[0][1]).toMatch(/order/);
   });
 });
