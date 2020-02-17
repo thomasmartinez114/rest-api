9bfb27394655b0b2f84ed925060e142a96ef91a1
tmartinez114 <tmartinez114@gmail.com>
2020-02-16 22:39:47 -0500

    init movieId

--- nodeJS/vidly/tests/integration/returns.test.js
+++ nodeJS/vidly/tests/integration/returns.test.js
@@ -5,18 +5,21 @@ describe('/api/returns', () => {
   // Load server and populate DB
   let server;
   let customerId;
+  let movieId;
 
   beforeEach(() => {
     server = require('../../index');
 
     customerId = mongoose.Types.ObjectId();
+    movieId = mongoose.Types.ObjectId();
 
     const rental = new Rental({
       customer: {
         _id: customerId,
         name: '12345',
         phone: '12345'
-      }
+      },
+      movie: {}
     });
   });
 
