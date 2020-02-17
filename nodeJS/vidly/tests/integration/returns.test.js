635573d8329d2442373ee38787e533e14bf40dc0
tmartinez114 <tmartinez114@gmail.com>
2020-02-17 18:53:34 -0500

    simplified expect to not have repetition of code

--- nodeJS/vidly/tests/integration/returns.test.js
+++ nodeJS/vidly/tests/integration/returns.test.js
@@ -131,10 +131,14 @@ describe('/api/returns', () => {
     const res = await exec();
 
     const rentalInDb = await Rental.findById(rental._id);
-    expect(res.body).toHaveProperty('dateOut');
-    expect(res.body).toHaveProperty('dateReturned');
-    expect(res.body).toHaveProperty('rentalFee');
-    expect(res.body).toHaveProperty('customer');
-    expect(res.body).toHaveProperty('movie');
+    // expect(res.body).toHaveProperty('dateOut');
+    // expect(res.body).toHaveProperty('dateReturned');
+    // expect(res.body).toHaveProperty('rentalFee');
+    // expect(res.body).toHaveProperty('customer');
+    // expect(res.body).toHaveProperty('movie');
+
+    expect(Object.keys(res.body)).toEqual(
+      expect.arrayContaining(['dateOut', 'dateReturned', 'rentalFee', 'customer', 'movie'])
+    );
   });
 });
