7aa678872ad9e652e416ac5cd8f4d44589df2799
tmartinez114 <tmartinez114@gmail.com>
2020-02-17 18:59:29 -0500

    added Joi to the test

--- nodeJS/vidly/routes/returns.js
+++ nodeJS/vidly/routes/returns.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const moment = require('moment');
 const { Rental } = require('../models/rental');
 const { Movie } = require('../models/movie');
@@ -6,8 +7,8 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', auth, async (req, res) => {
-  if (!req.body.customerId) return res.status(400).send('customerId not provided');
-  if (!req.body.movieId) return res.status(400).send('movieId not provided');
+  const { error } = validateReturn(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
   const rental = await Rental.findOne({
     'customer._id': req.body.customerId,
