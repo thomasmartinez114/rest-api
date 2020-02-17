6b1088fcac521b5e6a5074a02f63a6d369ab323d
tmartinez114 <tmartinez114@gmail.com>
2020-02-16 22:23:41 -0500

    postive cases

--- nodeJS/vidly/x.js
+++ nodeJS/vidly/x.js
@@ -1,7 +1,15 @@
 // POST /api/returns {customerId, movieId}
 
+// ----------- NEGATIVE CASES --------------------
 // Return 401 if client is not logged in - Unauthorized
 // Return 400 if customerId is not provided
 // Return 400 if movieId is not provided
 // Return 404 if no rental found for this customer/movie - NOT FOUND
 // Return 400 if rental already processed
+
+// ----------- POSITIVE CASES --------------------
+// Return 200 if valid request
+// Set the return date
+// Calculate rental fee
+// Increase the stock of movie
+// Return the rental & summary (dateout, dateReturned, rentalReceived)
