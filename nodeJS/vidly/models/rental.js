e415939bcae613974fbfcff56cf4c376748a2648
tmartinez114 <tmartinez114@gmail.com>
2020-02-17 19:18:40 -0500

    added instance method - moved rental fee calculation

--- nodeJS/vidly/models/rental.js
+++ nodeJS/vidly/models/rental.js
@@ -63,6 +63,15 @@ rentalSchema.statics.lookup = function(customerId, movieId) {
   });
 };
 
+// Instance method
+rentalSchema.methods.return = function() {
+  this.dateReturned = new Date();
+
+  // Calculation of rental fee
+  const rentalDays = moment().diff(this.dateOut, 'days');
+  this.rentalFee = rentalDays * this.movie.dailyRentalRate;
+};
+
 const Rental = mongoose.model('Rental', rentalSchema);
 
 function validateRental(rental) {
--- nodeJS/vidly/routes/returns.js
+++ nodeJS/vidly/routes/returns.js
@@ -14,9 +14,7 @@ router.post('/', [auth, validate(validateReturn)], async (req, res) => {
 
   if (rental.dateReturned) return res.status(400).send('Return already processed.');
 
-  rental.dateReturned = new Date();
-  const rentalDays = moment().diff(rental.dateOut, 'days');
-  rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
+  rental.return();
   await rental.save();
 
   await Movie.update(
